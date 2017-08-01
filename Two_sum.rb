e12dcc2e811e381d267e9542481af87373d22272
Waynee <xiewayne1993@gmail.com>
2017-08-01 17:49:32 +0800

    initial commit

---
+++ Two_sum.rb
@@ -0,0 +1,24 @@
+def two_sum(nums, target)
+    arr = []
+
+    nums.each do |num|
+        if nums.include?(target - num)
+
+            if target == num * 2
+                arr_select = nums.each_index.select { |i| nums[i] == num }
+                if arr_select.size > 1
+                  arr << arr_select[0]
+                  arr << arr_select[1]
+                  break
+                end
+            elsif target != num * 2
+                arr << nums.index(num)
+                arr << nums.index(target - num)
+                break
+            end
+        end
+    end
+    arr
+end
+
+p two_sum([3,2,4],6)
