794850934bcb96be54ca71103d112df5b64b2712
Waynee <xiewayne1993@gmail.com>
2017-08-17 19:25:29 -0400

    Longest Substring Without Repeating Characters

--- test.rb
+++ test.rb
@@ -1,40 +1,24 @@
-def add_two_numbers(l1, l2)
-  sum = 0
-  l_last = nil
-  l_return = nil
-  while !l1.nil? || !l2.nil?
-    num1 = l1.nil? ? 0 : l1.val
-    num2 = l2.nil? ? 0 : l2.val
-    sum = num1 + num2 + sum / 10
-    l1 = l1.next unless l1.nil?
-    l2 = l2.next unless l2.nil?
-    current_result = ListNode.new(sum % 10)
-    l_return.next = current_result unless l_return.nil?
-    l_return = current_result
-    l_last = l_return if l_last.nil?
+def length_of_longest_substring(s)
+  return nil if s.nil?
+  return 0 if s == ""
+  word_max = ""
+  str = ''
+  s.chars.each do |word|
+     if str.include?(word)
+       if word_max.length < str.length
+          word_max = str
+
+       end
+       str = str[str.index(word)+1..-1]
+       str << word
+     else
+        str << word
+        p str
+     end
   end
-  if sum / 10 == 1
-    l_return.next = ListNode.new(1) unless l_return.nil?
-  end
-  l_last
+  word_max.length
 end
 
-class ListNode
-    attr_accessor :val, :next
-    def initialize(val)
-        @val = val
-        @next = nil
-    end
-end
-
-l1 = ListNode.new(0)
-
-l2 = ListNode.new(0)
-
-
-
-l3 = add_two_numbers(l1, l2)
-while !l3.nil?
-  puts l3.val
-  l3 = l3.next
-end
+puts length_of_longest_substring("vorzzhimkumepoqlgwevcycfwiovxgksxdtwlcixy")
+puts length_of_longest_substring("bbbbbbbb")
+puts length_of_longest_substring("pwwkew")
